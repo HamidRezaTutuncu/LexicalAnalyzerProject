07a0154ed713e690e370f340d8873da492db79ab
Hamid Tutuncu <hamid.tt711@gmail.com>
2026-05-10 00:28:21 +0300

    Modüler Lexical Analyzer çekirdek mantığı ve Tkinter GUI arayüzü eklendi

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-# This is a sample Python script.
+import tkinter as tk
+from tkinter import messagebox, font
+from analyzer import LexicalAnalyzer
+
+
+class LexicalGui:
+    def __init__(self, root):
+        self.analyzer = LexicalAnalyzer()
+        self.root = root
+        self.root.title("Rumeli Üniv. - Lexical Analyzer")
+        self.root.geometry("500x350")
+        self.root.configure(bg="#f4f7f6")  # Soft arka plan
+
+        # Fontlar (Hocanın istediği Calibri tarzı)
+        self.title_font = font.Font(family="Calibri", size=16, weight="bold")
+        self.label_font = font.Font(family="Calibri", size=12)
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
