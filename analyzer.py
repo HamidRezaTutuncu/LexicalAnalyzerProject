e011d72286ad3339f7616a712e16b711fd951b57
Hamid Tutuncu <hamid.tt711@gmail.com>
2026-05-10 00:22:15 +0300

    analiz dosyasinda gerekli methodlar yazildi test edildi calisir durumda

---
+++ analyzer.py
@@ -0,0 +1,29 @@
+class LexicalAnalyzer:
+    def __init__(self):
+        self.keywords = ["int", "for", "while", "class", "return", "if", "else", "break", "continue"]
+
+    def is_letter(self, char):
+        return 'a' <= char <= 'z' or 'A' <= char <= 'Z'
+
+    def is_digit(self, char):
+        return '0' <= char <= '9'
+
+    def is_underscore(self, char):
+        return char == '_'
+
+    def check_variable(self, s):
+        if not s:
+            return False, "Input boş olamaz."
+
+        if s in self.keywords:
+            return False, f"'{s}' bir anahtar kelimedir, değişken olamaz."
+
+        if not self.is_letter(s[0]):
+            return False, "Değişken ismi harf ile başlamalıdır."
+
+        for char in s:
+            if not (self.is_letter(char) or self.is_digit(char) or self.is_underscore(char)):
+                return False, f"'{char}' geçersiz bir karakterdir."
+
+        return True, "Geçerli bir değişken ismidir."
+
